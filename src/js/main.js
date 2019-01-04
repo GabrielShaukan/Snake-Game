a3680935268befca8475c812cef8c7d978d613ab
GabrielShaukan <gabrielshaukan@yahoo.com>
2019-01-04 13:02:37 +0100

    Added controls for snake object

--- src/js/main.js
+++ src/js/main.js
@@ -12,7 +12,17 @@ function draw() {
   snake.show();
 }
 
+//Sets up controls for snake
 function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    snake.dir(0,-1);
+  } else if (keyCode === DOWN_ARROW) {
+    snake.dir(0,1);
+  } else if (keyCode === RIGHT_ARROW){
+    snake.dir(1,0)
+  } else if (keyCode === LEFT_ARROW) {
+    snake.dir(-1,0)
+  }
 }
 
 
--- src/js/snake.js
+++ src/js/snake.js
@@ -1,14 +1,24 @@
+//Snake object
+
 function snake(x, y, xSpeed, ySpeed) {
   this.x = 0;
   this.y = 0;
   this.xSpeed = 1;
   this.ySpeed = 0;
 
+  //changes snake direction
+  this.dir = function(x,y) {
+    this.xSpeed = x;
+    this.ySpeed = y;
+  }
+
+  //makes snake move
   this.update = function() {
     this.x = this.x + this.xSpeed;
     this.y = this.y + this.ySpeed;
   }
 
+  //draws snake  on canvas
   this.show = function() {
     fill(255);
     rect(this.x, this.y, 10, 10);
